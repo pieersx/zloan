02bcd531b234629141939e2b5a182c8969f227f9
pieers <piers_84@hotmail.com>
2024-10-23 01:43:02 -0500

    Add comision

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 function generatorLoan() {
     document.getElementById("tab").innerHTML = "";
-    const capital = Number(document.getElementById("capital").value);
-    const cuota = Number(document.getElementById("cuota").value);
-    const interes = Number(document.getElementById("intereses").value);
-    const comision = Number(document.getElementById("comision").value);  // Nuevo campo para la comisión mensual
+    const P = Number(document.getElementById("capital").value);
+    const n = Number(document.getElementById("cuota").value);
+    const c = Number(document.getElementById("comision").value);
+    let i = Number(document.getElementById("intereses").value)/100;
 
-    if (capital > 0) {
-        let tem = Math.pow(1 + (interes / 100), 1 / 12) - 1;
-        let saldo = capital;
-        let totalInteres = 0;
-        let totalAmortizacion = 0;
-        let MontoCuota = 0;
-        let totalComision = 0;  // Variable para el total de comisiones
+    const tipoTasa = String(document.getElementById("tipoTasa").value);
+    if (tipoTasa === "TEA") {
+        i = Math.pow(1 + i, 1/12) - 1;
+    }
 
-        for (let mes = 0; mes <= cuota; ++mes) {
+    if (P > 0) {
+        let saldo = P;
+        let interesTotal = 0;
+        let amortizacionTotal = 0;
+        let cuotaTotal = 0;
+        let comisionTotal = 0;
+        for (let mes = 0; mes <= n; ++mes) {
             if (mes == 0) {
-                document.getElementById("tab").innerHTML +=
+                document.getElementById("tab").innerHTML = document.getElementById("tab").innerHTML +
                 `<tr>
                     <td>${mes}</td>
                     <td></td>
@@ -24,42 +27,32 @@ function generatorLoan() {
                     <td>S/ ${saldo.toFixed(2)}</td>
                 </tr>`;
             } else {
-                const totalCuota = (capital * tem / (1 - Math.pow(1 + tem, -cuota))) + comision;
-                const totalCuotaRed = totalCuota.toFixed(2);
-
-                const interesSaldo = saldo * tem;
-                const interesSaldoRed = interesSaldo.toFixed(2);
-
-                const amortizacion = totalCuota - interesSaldo - comision;
-                const amortizacionRed = amortizacion.toFixed(2);
-
+                const Cuota = P * (Math.pow(1 + i, n) * i) / (Math.pow(1 + i, n) - 1) + c;
+                const interes = saldo * i;
+                const amortizacion = Cuota - interes - c;
                 saldo -= amortizacion;
 
-                document.getElementById("tab").innerHTML +=
-                            `<tr>
-                                <td>${mes}</td>
-                                <td>S/ ${totalCuotaRed}</td>
-                                <td>S/ ${interesSaldoRed}</td>
-                                <td>S/ ${amortizacionRed}</td>
-                                <td>S/ ${Math.abs(saldo.toFixed(2))}</td>
-                                <td>S/ ${comision}</td>
-                            </tr>`;
+                document.getElementById("tab").innerHTML = document.getElementById("tab").innerHTML +
+                `<tr>
+                    <td>${mes}</td>
+                    <td>S/ ${Cuota.toFixed(2)}</td>
+                    <td>S/ ${Math.round(interes).toFixed(2)}</td>
+                    <td>S/ ${amortizacion.toFixed(2)}</td>
+                    <td>S/ ${Math.abs(saldo.toFixed(2))}</td>
+                    <td>S/ ${c.toFixed(2)}</td>
+                </tr>`;
 
-                totalInteres += interesSaldo;
-                totalAmortizacion += amortizacion;
-                MontoCuota += totalCuota;
-                totalComision += comision;
+                interesTotal += interes;
+                amortizacionTotal += amortizacion;
+                cuotaTotal += Cuota;
+                comisionTotal += c;
             }
         }
 
-        let totalInteresRed = Math.round(totalInteres.toFixed(2));
-        let totalMontoCuota = Math.round(MontoCuota.toFixed(2));
-        let totalAmortizacionRed = totalAmortizacion.toFixed(2);
-
-        document.getElementById("t1").innerHTML = "S/ " + totalMontoCuota.toFixed(2);
-        document.getElementById("t2").innerHTML = "S/ " + totalInteresRed.toFixed(2);
-        document.getElementById("t3").innerHTML = "S/ " + totalAmortizacionRed;
-        document.getElementById("t5").innerHTML = "S/ " + totalComision.toFixed(2);  // Mostrar el total de comisiones
+        document.getElementById("t1").innerHTML = "S/ " + Math.round(cuotaTotal.toFixed(2)).toFixed(2);
+        document.getElementById("t2").innerHTML = "S/ " + Math.round(interesTotal.toFixed(2)).toFixed(2);
+        document.getElementById("t3").innerHTML = "S/ " + amortizacionTotal.toFixed(2);
+        document.getElementById("t5").innerHTML = "S/ " + comisionTotal.toFixed(2);
     } else {
         alert("Falta ingresar los números");
     }
